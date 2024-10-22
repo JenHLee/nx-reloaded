1f3f1feb77791a61202dd46e5fea4ef3ec6dbf65
Hyunju Jennie Lee <hj.jennie.lee@gmail.com>
2024-10-21 22:00:30 -0600

    create firestore and connect

--- src/routes/firebase.ts
+++ src/routes/firebase.ts
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBaZo0bvSBNuPJkpxL6NmOMyDXEeU5pz-E",
@@ -14,3 +16,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
